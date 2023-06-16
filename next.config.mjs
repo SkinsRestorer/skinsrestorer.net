0b412ee9f1969214a36acd9741015dccc3e5bde4
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2023-06-16 21:57:55 +0200

    Add some security headers

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,33 @@ const withNextra = nextra({
     defaultShowCopyCode: true
 })
 
+const securityHeaders = [
+    {
+        key: 'X-DNS-Prefetch-Control',
+        value: 'on'
+    },
+    {
+        key: 'X-XSS-Protection',
+        value: '1; mode=block'
+    },
+    {
+        key: 'X-Frame-Options',
+        value: 'SAMEORIGIN'
+    },
+    {
+        key: 'X-Content-Type-Options',
+        value: 'nosniff'
+    },
+    {
+        key: 'Content-Security-Policy',
+        value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; object-src 'none'; base-uri 'self'; connect-src 'self' https://discord.com; font-src 'self'; frame-src 'self'; img-src 'self' data:; manifest-src 'self'; media-src 'self'; worker-src 'self';"
+    },
+    {
+        key: 'Clear-Site-Data',
+        value: "'storage'" // To wipe service workers, remove this if it causes problems
+    }
+]
+
 export default withNextra({
     reactStrictMode: true,
     redirects: async () => {
@@ -17,6 +44,14 @@ export default withNextra({
                 destination: process.env.NEXT_PUBLIC_DISCORD_LINK,
                 permanent: false,
             },
+        ]
+    },
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: securityHeaders
+            }
         ]
     },
     images: {
