9431c3dfd3c2566c53aef43d5c2cf54d18ae6de9
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2023-06-16 21:49:05 +0200

    Remove assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './assets/**/*.{css}',
     './theme.config.tsx'
   ],
   theme: {
