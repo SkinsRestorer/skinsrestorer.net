923e0d3228d3483a45eb7343405df237e9d62afb
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-09-20 17:40:38 +0200

    feat: further improvements

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -2,10 +2,12 @@ import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
 import { CodeBlock, Pre } from 'fumadocs-ui/components/codeblock';
 import { Mermaid } from '~/components/mdx/mermaid';
+import { ImageZoom } from 'fumadocs-ui/components/image-zoom';
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    img: (props) => <ImageZoom {...(props as any)} />,
     // HTML `ref` attribute conflicts with `forwardRef`
     pre: ({ ref: _ref, ...props }) => (
       <CodeBlock {...props}>
