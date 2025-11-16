b43983809e5d6d56ba2efac0a0dee7c8a247fbdf
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-11-16 19:14:29 +0100

    fix: resolve warning

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,6 +1,6 @@
 import posthog from "posthog-js";
 
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
   api_host: "/RELAY-KAWD",
   ui_host: "https://eu.posthog.com",
   defaults: "2025-05-24",
