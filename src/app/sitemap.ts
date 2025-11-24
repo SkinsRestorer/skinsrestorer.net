b6d1c387b5213c9f6e7de3ddc210ddd5661ffbb4
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-11-24 16:33:42 +0100

    feat: add docs to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,10 +1,25 @@
 import type { MetadataRoute } from "next";
+import { source } from "@/lib/source";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  return (process.env.SITEMAP_PAGES || "").split("|").map((page) => ({
-    url: `https://skinsrestorer.net${page}`,
-    lastModified: new Date(),
-    changeFrequency: "daily",
-    priority: 0.7,
-  }));
+  return [
+    ...(process.env.SITEMAP_PAGES || "").split("|").map(
+      (page) =>
+        ({
+          url: `https://skinsrestorer.net${page}`,
+          lastModified: new Date(),
+          changeFrequency: "daily",
+          priority: 0.7,
+        }) satisfies MetadataRoute.Sitemap[number],
+    ),
+    ...source.getPages().map((page) => {
+      const { lastModified } = page.data;
+      return {
+        url: `https://skinsrestorer.net${page.url}`,
+        lastModified: lastModified ? new Date(lastModified) : undefined,
+        changeFrequency: "weekly",
+        priority: 0.5,
+      } satisfies MetadataRoute.Sitemap[number];
+    }),
+  ];
 }
