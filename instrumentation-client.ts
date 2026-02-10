c5146aea8d10aaf654e01c1545c06f4a6201f0cc
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2026-02-10 20:43:49 +0100

    chore: update api host

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -4,7 +4,8 @@ const apiKey = process.env.NEXT_PUBLIC_POSTHOG_KEY;
 
 if (typeof window !== "undefined" && apiKey) {
   posthog.init(apiKey, {
-    api_host: "/RELAY-KAWD",
+    // api_host: "/RELAY-KAWD",
+    api_host: "https://e.skinsrestorer.net",
     ui_host: "https://eu.posthog.com",
     defaults: "2025-05-24",
     capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
