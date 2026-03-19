5923f2d44e29e2731fcf347ba8dcff129e49e494
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2026-03-19 20:44:36 +0100

    fix: migrate back to direct url

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -5,7 +5,7 @@ const apiKey = process.env.NEXT_PUBLIC_POSTHOG_KEY;
 if (typeof window !== "undefined" && apiKey) {
   posthog.init(apiKey, {
     // api_host: "/RELAY-KAWD",
-    api_host: "https://e.skinsrestorer.net",
+    api_host: "https://eu.i.posthog.com",
     ui_host: "https://eu.posthog.com",
     defaults: "2025-05-24",
     capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
