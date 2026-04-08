8fc4ef04f66f3a2e4721c6e35a786e5c6e765e22
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2026-04-08 15:26:07 +0200

    chore: update components

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,18 +1,26 @@
+import { Accordion, Accordions } from "fumadocs-ui/components/accordion";
+import { Callout } from "fumadocs-ui/components/callout";
 import { CodeBlock, Pre } from "fumadocs-ui/components/codeblock";
+import * as FilesComponents from "fumadocs-ui/components/files";
 import { ImageZoom } from "fumadocs-ui/components/image-zoom";
+import { Step, Steps } from "fumadocs-ui/components/steps";
+import * as TabsComponents from "fumadocs-ui/components/tabs";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
+import type { ComponentProps, FC } from "react";
 import { Mermaid } from "@/components/mdx/mermaid";
-
-const MdxImage: NonNullable<MDXComponents["img"]> = (componentProps) => {
-  const { ref: _ref, ...props } = componentProps;
-  return <ImageZoom {...props} />;
-};
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
-    img: MdxImage,
+    Accordion,
+    Accordions,
+    Step,
+    Steps,
+    ...TabsComponents,
+    ...FilesComponents,
+    blockquote: Callout as unknown as FC<ComponentProps<"blockquote">>,
+    img: ImageZoom as unknown as FC<ComponentProps<"img">>,
     // HTML `ref` attribute conflicts with `forwardRef`
     pre: ({ ref: _ref, ...props }) => (
       <CodeBlock {...props}>
