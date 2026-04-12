e387fdf0006a41e74867025d89f3dc75295ae934
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2026-04-12 12:10:31 +0200

    chore: switch to lucide icons plugin

--- src/lib/source.ts
+++ src/lib/source.ts
@@ -1,19 +1,11 @@
 import { docs } from "fumadocs-mdx:collections/server";
 import { type InferPageType, loader } from "fumadocs-core/source";
-import { icons } from "lucide-react";
-import { createElement } from "react";
+import { lucideIconsPlugin } from "fumadocs-core/source/lucide-icons";
 
 export const source = loader({
   baseUrl: "/docs",
   source: docs.toFumadocsSource(),
-  icon(icon) {
-    if (!icon) {
-      // You may set a default icon
-      return;
-    }
-
-    if (icon in icons) return createElement(icons[icon as keyof typeof icons]);
-  },
+  plugins: [lucideIconsPlugin()],
 });
 
 export function getPageImage(page: InferPageType<typeof source>) {
